97e3d88d414cae0d55c6cb67ac0aceb0b2c841c2
Tea Tran <tatran@seas.upenn.edu>
2020-04-14 20:00:36 -0400

    refactored Tractor

--- Assets/Scripts/Tractor.cs
+++ Assets/Scripts/Tractor.cs
@@ -31,18 +31,21 @@ public class Tractor : MonoBehaviour
         if (hasPlayer && fuelLeft > 0 && 
             (Input.GetKey(Controller.kbMoveLeft) || Input.GetKey(Controller.kbMoveRight) ||
              Input.GetKey(Controller.kbMoveForward) || Input.GetKey(Controller.kbMoveBackward)))
-        {
-            if (Input.GetKey(Controller.kbMoveLeft))
-                transform.position -= new Vector3(Time.deltaTime * speed, 0, 0);
-            else if (Input.GetKey(Controller.kbMoveRight))
-                transform.position += new Vector3(Time.deltaTime * speed, 0, 0);
-            if (Input.GetKey(Controller.kbMoveForward))
-                transform.position += new Vector3(0, 0, Time.deltaTime * speed);
-            else if (Input.GetKey(Controller.kbMoveBackward))
-                transform.position -= new Vector3(0, 0, Time.deltaTime * speed);
+            HandleMovement();
+    }
 
-            fuelLeft -= (Time.deltaTime % 1) * fuelDepletePerSec;
-        }
+    private void HandleMovement()
+    {
+        if (Input.GetKey(Controller.kbMoveLeft))
+            transform.position -= new Vector3(Time.deltaTime * speed, 0, 0);
+        else if (Input.GetKey(Controller.kbMoveRight))
+            transform.position += new Vector3(Time.deltaTime * speed, 0, 0);
+        if (Input.GetKey(Controller.kbMoveForward))
+            transform.position += new Vector3(0, 0, Time.deltaTime * speed);
+        else if (Input.GetKey(Controller.kbMoveBackward))
+            transform.position -= new Vector3(0, 0, Time.deltaTime * speed);
+
+        fuelLeft -= (Time.deltaTime % 1) * fuelDepletePerSec;
     }
 
     private void OnCollisionStay(Collision collision)
