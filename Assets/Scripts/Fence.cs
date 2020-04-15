4a4d909615a16c6ab0e26abb44a9c494eea8599b
LAPTOP-GE95V166\Janine <jliu99@seas.upenn.edu>
2020-04-14 21:03:58 -0400

    Add fences

--- Assets/Scripts/Fence.cs
+++ Assets/Scripts/Fence.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Fence : MonoBehaviour
 {
+
+    float health;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 100.0f;
     }
 
     // Update is called once per frame
