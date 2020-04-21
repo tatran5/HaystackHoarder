7157d68425206704f0d73e0d394e7f6837e536db
LAPTOP-GE95V166\Janine <jliu99@seas.upenn.edu>
2020-04-20 23:53:37 -0400

    Finish grid system, grid-based wander behavior, and fence avoidance

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Animal : MonoBehaviour
 {
     Global globalObj;
-    public Vector2Int gridPos;
 
     // Variables related to the feed meter that depletes with time
     public float feedMeter;
@@ -16,8 +15,10 @@ public class Animal : MonoBehaviour
     public float weight;        // Varies per animal
     public float speed;         // Speed at which the animal runs
 
-    public int wanderLength;
-    public int wanderTimer;
+
+    // Variables for naive wander behavior
+    int wanderLength;
+    int wanderTimer;
 
     public Vector2 wanderPoint;
     public Vector3 wanderDirection;
@@ -31,48 +32,68 @@ public class Animal : MonoBehaviour
         feedTickLength = 75;
         feedTimer = 0;
 
-        weight = 10.0f;
-        speed = 1.0f;
-
         wanderLength = 200;
         wanderTimer = 0;
-        wanderDirection = Vector3.zero;
 
+        speed = 2.0f;
+
+        wanderPoint = Vector2.zero;
+        wanderDirection = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Feed Meter
-
+        // Update feed Meter
         feedTimer += 1;
         if (feedTimer >= feedTickLength) {
             feedMeter -= 1.0f;
             feedTimer = 0;
         }
+    }
 
+    void FixedUpdate() {
         // Wander Behavior
         Move();
     }
 
-    void FeedAnimal() {
+    public void FeedAnimal() {
         feedMeter = 100.0f;
         feedTimer = 0;
     }
 
+    bool PositionEquality(Vector2 pos1, Vector2 pos2) {
+        return Mathf.Abs(pos1.x - pos2.x) < 0.25f && Mathf.Abs(pos1.y - pos2.y) < 0.25f;
+    }
+
     void Move() {
+
+        Vector2 currentPos = new Vector2(gameObject.transform.position.x, gameObject.transform.position.z);
+
         if (wanderDirection == Vector3.zero) {
             float wanderX = Random.Range(-1.0f, 1.0f);
             float wanderZ = Random.Range(-1.0f, 1.0f);
             wanderDirection = new Vector3(wanderX, 0.0f, wanderZ);
+
+            // Ray to cast along
+            Vector2 wanderDir = new Vector2(wanderDirection.x, wanderDirection.z);
+
+            Vector2 targetPos = Vector2.zero;
+            int targetIndex = globalObj.grid_raycastFromPoint(currentPos, wanderDir);
+            if (targetIndex >= 0) {
+                wanderPoint = globalObj.grid_getCenterOfCell(targetIndex);
+                wanderDirection = new Vector3(wanderPoint.x, 0.0f, wanderPoint.y)
+                                    - new Vector3(currentPos.x, 0.0f, currentPos.y);
+                wanderDirection = wanderDirection.normalized;
+            }
+            else {
+                wanderDirection = Vector3.zero;
+            }
         }
 
-        wanderTimer += 1;
-
-        if (wanderTimer >= wanderLength)
+        if (PositionEquality(wanderPoint, currentPos))
         {
-            wanderDirection = Vector2.zero;
-            wanderTimer = 0;
+            wanderDirection = Vector3.zero;
         }
         else {
             gameObject.transform.position += wanderDirection * speed * Time.deltaTime;
