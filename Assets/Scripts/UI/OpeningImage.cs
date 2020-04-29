1356a58ea3abdc3b24a7bd39151a0ab1777d97c0
LAPTOP-GE95V166\Janine <jliu99@seas.upenn.edu>
2020-04-29 00:11:33 -0400

    Create painting fade-in

---
+++ Assets/Scripts/UI/OpeningImage.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpeningImage : MonoBehaviour
+{
+    
+    public Image img;
+    float fade;
+    float difference;
+
+    int waitTimer;
+    int waitTimerLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fade = 1;
+        difference = 0.2f;
+
+        waitTimer = 0;
+        waitTimerLength = 100;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (waitTimer < waitTimerLength)
+        {
+            waitTimer += 1;
+        }
+        else {
+            fade -= difference * Time.deltaTime;
+            if (fade < 0)
+            {
+                fade = 0;
+            }
+
+            Color temp = img.color;
+            temp.a = fade;
+            img.color = temp;
+        }
+    }
+}
